c03683e16c6dcd65d64b301246136be9ed662b56
Aholu Samuel <samuelaholu15@gmail.com>
2022-01-29 09:17:35 +0100

    Text area overflow: hidden

--- home/post.php
+++ home/post.php
@@ -181,7 +181,7 @@ include("components/mobile.php");
                 <div class="" style="">
                     <div class="col-12 row">
                         <div class="" style="width: 80%; margin-left: 0px; margin-right:" >
-                        <textarea name="share" class="" style="border-radius: 20px; background-color: #e6e6e6; width: 100%; height: 40px; padding: 10px; border-color: #999999;" aria-disabled="true" placeholder=" Post Comment!" id="content"></textarea>
+                        <textarea name="share" class="" style="border-radius: 20px; background-color: #e6e6e6; width: 100%; height: 40px; padding: 10px; border-color: #999999; overflow: hidden;" aria-disabled="true" placeholder=" Post Comment!" id="content"></textarea>
                         </div>
                         <div class="" style="width: 20%;">
                             <button type="button" id="comment_btn"  class="post-share-btn">Post</button>
