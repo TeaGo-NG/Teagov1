cc5598aa086f659570228698d70a88d1671a6fa6
Greatness Prodigy <49532326+Grtnxhor@users.noreply.github.com>
2022-05-01 02:49:10 +0100

    kyc

---
+++ kyc/components/footer.php
@@ -0,0 +1,14 @@
+  <!-- Footer -->
+  <footer class="content-footer footer bg-footer-theme">
+              <div class="container-xxl text-dark d-flex flex-wrap justify-content-center py-2 flex-md-row flex-column">
+                <div class="mb-2 mb-md-0">
+                  &copy; TeaGo Nig
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  |   Developed by
+                  <a href="https://hynitr.com" class="footer-link" target="_blank">Hynitr</a>
+                </div>
+              </div>
+            </footer>
+            <!-- / Footer -->
---
+++ kyc/welcome.php
@@ -0,0 +1,83 @@
+<?php 
+include("components/head.php");
+?>
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+
+    
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+
+          <?php include("components/nav.php"); ?>
+
+          <!-- / Navbar -->
+
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <!-- Content -->
+
+            <div class="container-xxl flex-grow-1 container-p-y">
+              <div class="row">
+                    <div class="col-lg-12 col-12 mb-4 order-0">
+                      <div class="card">
+                        <div class="d-flex align-items-end row">
+                          <div class="col-sm-7">
+                            <div class="card-body">
+                              <h5 class="card-title text-primary">Welcome <?php echo Ucwords($_SESSION['user']) ?>! 🎉</h5>
+                              <p class="mb-4">
+                                You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
+                                your profile.
+                              </p>
+
+                              <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+                            </div>
+                          </div>
+                          <div class="col-sm-5 text-center text-sm-left">
+                            <div class="card-body pb-0 px-0 px-md-4">
+                              <img
+                                src="assets/img/illustrations/man-with-laptop-light.png"
+                                height="140"
+                                alt="View Badge User"
+                                data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                                data-app-light-img="illustrations/man-with-laptop-light.png"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+              </div>
+            </div>
+            <!-- / Content -->
+
+            <?php include("components/footer.php"); ?>
+
+            <div class="content-backdrop fade"></div>
+          </div>
+          <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
+      </div>
+
+    
+
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="assets/vendor/libs/popper/popper.js"></script>
+    <script src="assets/vendor/js/bootstrap.js"></script>
+    <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
